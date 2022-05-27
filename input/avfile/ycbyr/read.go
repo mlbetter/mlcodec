bc790a16aef6ebfe5722ad9d14d69452258ea525
Buddruggy <lwzmyhigh@outlook.com>
2022-05-28 00:21:47 +0800

    add: add read yuv420 file code

--- input/avfile/ycbyr/read.go
+++ input/avfile/ycbyr/read.go
@@ -2,7 +2,11 @@ package ycbyr
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"image"
+	"io"
 	"log"
 	"os"
 )
@@ -40,13 +44,77 @@ func (y *YCbCr) Read() error {
 	log.Println(r.Size())
 
 	// todo: read buffer by video fmt
-	return nil
+	switch y.format {
+	case image.YCbCrSubsampleRatio420:
+		return y.readYUV420(r)
+	default:
+		log.Println("not surpport now")
+		return fmt.Errorf("not surrport now")
+	}
 }
 
 func (y *YCbCr) GetOneFrame() *image.YCbCr {
-	return nil
+	f := y.frames[y.frameIndex]
+	y.frameIndex++
+	if y.frameIndex >= y.frameCnt {
+		y.frameIndex = 0
+	}
+	return f
 }
 
 func (y *YCbCr) GetFrameCnt() int {
-	return 0
+	return y.frameCnt
+}
+
+func (y *YCbCr) readYUV420(r *bufio.Reader) error {
+	bufIter := make([]byte, y.weight*y.height*3/2*8)
+
+	for {
+		n, err := r.Read(bufIter)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			log.Println("read file done")
+			break
+		}
+		if n != y.weight*y.height*2 {
+			log.Println("read length error, len:", n)
+			return fmt.Errorf("read file error")
+		}
+
+		imageIter := image.NewYCbCr(image.Rectangle{
+			Min: image.Point{X: 0, Y: 0},
+			Max: image.Point{X: y.weight, Y: y.height},
+		}, image.YCbCrSubsampleRatio420)
+
+		i := 0
+		// y
+		for i < y.weight*y.height*8 {
+			imageIter.Y[i] = BytesToUINT8(bufIter[i : i+8])
+			i = i + 8
+		}
+
+		// u
+		for i < y.weight*y.height/4*8 {
+			imageIter.Cb[i] = BytesToUINT8(bufIter[i : i+8])
+			i = i + 8
+		}
+
+		// v
+		for i < y.weight*y.height/4*8 {
+			imageIter.Cr[i] = BytesToUINT8(bufIter[i : i+8])
+			i = i + 8
+		}
+
+		y.frames = append(y.frames, imageIter)
+	}
+	return nil
+}
+
+func BytesToUINT8(bys []byte) uint8 {
+	bytebuff := bytes.NewBuffer(bys)
+	var data uint8
+	binary.Read(bytebuff, binary.BigEndian, &data)
+	return data
 }
